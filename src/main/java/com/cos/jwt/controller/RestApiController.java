2e2025500425828c12427f043b56f13d9a087d38
hyunmin0317 <choihm9903@naver.com>
2024-07-28 16:39:02 +0900

    feat: 회원가입 API (#9)

--- src/main/java/com/cos/jwt/controller/RestApiController.java
+++ src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,11 +1,20 @@
 package com.cos.jwt.controller;
 
+import com.cos.jwt.entity.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class RestApiController {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
 
     @GetMapping("/home")
     public String home() {
@@ -15,5 +24,14 @@ public class RestApiController {
     @PostMapping("/token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    @PostMapping("/join")
+    public String join(@RequestBody User user) {
+        String encPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setRoles("ROLE_USER");
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "회원가입 완료";
     }
 }
