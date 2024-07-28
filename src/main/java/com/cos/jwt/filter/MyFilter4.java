c92e408b4f6d88c5f69d4d3d5f55839a483b900a
hyunmin0317 <choihm9903@naver.com>
2024-07-28 12:40:14 +0900

    feat: 예시 필터 (#6)

---
+++ src/main/java/com/cos/jwt/filter/MyFilter4.java
@@ -0,0 +1,15 @@
+package com.cos.jwt.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class MyFilter4 implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("필터4");
+        chain.doFilter(request, response);
+    }
+}
