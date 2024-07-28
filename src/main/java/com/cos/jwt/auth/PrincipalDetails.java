185957f55acf3d15476ea09189445acb51d5fbde
hyunmin0317 <choihm9903@naver.com>
2024-07-28 16:25:33 +0900

    feat: JWT 인증 필터 (#8)

---
+++ src/main/java/com/cos/jwt/auth/PrincipalDetails.java
@@ -0,0 +1,33 @@
+package com.cos.jwt.auth;
+
+import com.cos.jwt.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoleList()
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
